c7f31b7274c3c5f83ba321a66e2e293b965235a5
Johana Wisar <111103556+Johanawisar@users.noreply.github.com>
2023-08-14 21:48:39 -0600

    Ñunes 1

---
+++ ionic-basic-1904333/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-basic-1904333',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
